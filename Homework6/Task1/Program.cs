046bdba5b7b3bcec90a2f77a264b5c035764ffd9
Alexey <slow.net@mail.ru>
2022-11-14 16:15:43 +0300

    solving the first task of homework number six

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите колличество чисел");
+bool isParsedNum = int.TryParse(Console.ReadLine(), out int number);
+if (!isParsedNum)
+{
+    Console.WriteLine("Не правильно введенны данные , повторите ввод");
+}
+
+Console.WriteLine("Введите числа");
+int sumPositivNumber = SumPositivNumber(number);
+Console.WriteLine(sumPositivNumber);
+int SumPositivNumber(int size)
+{
+    int[] array = new int[size];
+    int moreThanZero = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        int dig = 0;
+        bool isParsedDig = false;
+        while (!isParsedDig)
+        {
+            isParsedDig = int.TryParse(Console.ReadLine(), out dig);
+            if (!isParsedDig)
+            {
+                Console.WriteLine("Не правильно введенны данные , повторите ввод");
+            }
+            else
+            {
+                array[i]=dig;
+                if (array[i]> 0) moreThanZero = moreThanZero + array[i];
+            }
+        }
+    }
+    return moreThanZero;
+}
+
