0bc9ebbe5827b043e3fdc0e4664b6edf6d83636d
Alexey <slow.net@mail.ru>
2022-11-14 12:08:25 +0300

    Solving the first task of homework number five

---
+++ Homework5/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите размер массива");
+bool isParsedSize = int.TryParse(Console.ReadLine(), out int sizeArray);
+if (!isParsedSize)
+{
+    Console.WriteLine("Вы ввели не корректные данные");
+    return;
+}
+int[] array = new int[sizeArray];
+FillArray(array);
+PrintArray(array);
+int numberOfEvenNumbers = NumberOfEvenNumbers(array);
+Console.WriteLine();
+Console.WriteLine(numberOfEvenNumbers);
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("]");
+}
+int NumberOfEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+void FillArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(100, 1000);
+    }
+}
