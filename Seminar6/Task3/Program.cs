a1983726a642b91faa126de5f75e932cd7478618
Alexey <slow.net@mail.ru>
2022-11-17 17:50:43 +0300

    solving the third problem from the seminar six

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число");
+int number=Convert.ToInt32(Console.ReadLine());
+double [] fibonachi= Fibonachi(number);
+PrintArray(fibonachi);
+double [] Fibonachi(int num)
+{
+    double [] array = new double[num];
+    array[0]=1;
+    array[1]=1;
+    for (int i = 2; i < array.Length; i++)
+    {
+      array[i]= array[i-1]+ array[i-2]; 
+    }
+    return array;
+}
+void PrintArray(double [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+
