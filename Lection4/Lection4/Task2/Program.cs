16e57434e79df974afb4f0c23a620b3e634f8fb8
Alexey <slow.net@mail.ru>
2022-11-18 17:58:13 +0300

    array painting

---
+++ Lection4/Lection4/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿int[,] matrix = new int[10, 10]
+{
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+};
+PrintMatrix(matrix);
+Console.WriteLine();
+FillMatrix(1, 1);
+PrintMatrix(matrix);
+void FillMatrix(int row, int col)
+{
+    if (matrix[row, col] == 0)
+    {
+        matrix[row, col] = 2;
+        FillMatrix(row - 1, col);
+        FillMatrix(row , col-1);
+        FillMatrix(row + 1, col);
+        FillMatrix(row , col+1);
+    }
+}
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] == 0)
+                Console.Write($" ");
+            if(matr[i,j]==2)
+            Console.Write($"V");
+            else
+                Console.Write($" + ");
+        }
+        Console.WriteLine();
+    }
+}
