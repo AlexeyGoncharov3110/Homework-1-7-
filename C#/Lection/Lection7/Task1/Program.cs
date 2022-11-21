c673ac70e115e956c140d1b448ff16e6b1401a0f
Alexey <slow.net@mail.ru>
2022-11-21 14:11:39 +0300

    example of solving a problem with factorial

---
+++ C#/Lection/Lection7/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 10;
+int factorial = Factorial(n);
+int Factorial(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n * Factorial(n - 1);
+}
+Console.WriteLine(factorial);
