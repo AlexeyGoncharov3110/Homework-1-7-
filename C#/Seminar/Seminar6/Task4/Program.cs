8515a6b8a1ffc902c5ac13fc7bc344b3848bf9cf
Alexey <slow.net@mail.ru>
2022-11-22 15:21:07 +0300

    solution to the first problem of homework number eight

---
+++ C#/Seminar/Seminar6/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32(Console.ReadLine());
+// int [] fibonachi=Fibonacci(num);
+// double [] FibonacciArray(int n)
+// {
+//     double [] fib = new double[n];
+//     return Fibonacci(fib,n);
+// }
+// double [] Fibonacci(double []fib,int n)
+// {
+//     if(n==1 || n==2) 
+//     {
+//         return fib[n-1]=1;
+//     }
+//     else 
+//     {
+//         if(fib[n-2]!=0)
+//         {
+//             if(fib[n-1]!=0)
+//             return fib[n-1]=fib[n-2]+fib[n-3];
+//             else
+//             return fib[n-1]=Fibonacci(fib,n-1) + fib[n-3];
+//         }
+//         else
+//         {
+//             return fib[n-1] = Fibonacci(fib,n-1) + Fibonacci(fib,n-2);
+//         }
+//     }
+// }
